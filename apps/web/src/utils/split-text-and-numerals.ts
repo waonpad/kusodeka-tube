f3cffa8e54d921e98826ca75d08233a1fb066b8d
waonpad <waonpad@gmail.com>
2023-11-29 22:12:18 +0900

    fix: 🐞 漢数字が見つからないと正常に分割されない問題を修正

--- apps/web/src/utils/split-text-and-numerals.ts
+++ apps/web/src/utils/split-text-and-numerals.ts
@@ -5,7 +5,11 @@ export const splitTextAndNumerals = (text: string) => {
   const findedKnajiNumbers = findKanjiNumbers(text);
   const sortedFindedKnajiNumbers = findedKnajiNumbers.sort((a, b) => b.length - a.length);
 
-  const splitByKanjiNumbers = text.split(new RegExp(`(${sortedFindedKnajiNumbers.join('|')})`));
+  // sortedFindedKnajiNumbersが空の場合は、textをそのまま返さないと一文字ずつ分割されてしまう
+  const splitByKanjiNumbers =
+    sortedFindedKnajiNumbers.length > 0
+      ? text.split(new RegExp(`(${sortedFindedKnajiNumbers.join('|')})`))
+      : [text];
 
   const splitByArabicNumbers = splitByKanjiNumbers
     .map((text) => {
