2866516da6ff13713c25f5b185e5c7e193182a5f
waonpad <waonpad@gmail.com>
2023-12-03 21:38:59 +0900

    fix: 🐞 命名違いを修正

--- packages/ui/components/ui/date-picker.tsx
+++ packages/ui/components/ui/date-picker.tsx
@@ -3,31 +3,30 @@
 import * as React from 'react';
 import { format } from 'date-fns';
 import { Calendar as CalendarIcon } from 'lucide-react';
-
 import { cn } from 'ui/lib/utils';
 import { Button } from 'ui/components/ui/button';
 import { Calendar } from 'ui/components/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from 'ui/components/ui/popover';
 
-export function DatePickerDemo() {
+export function DatePicker() {
   const [date, setDate] = React.useState<Date>();
 
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button
-          variant={'outline'}
           className={cn(
             'w-[280px] justify-start text-left font-normal',
             !date && 'text-muted-foreground'
           )}
+          variant="outline"
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
           {date ? format(date, 'PPP') : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
-        <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+        <Calendar initialFocus mode="single" onSelect={setDate} selected={date} />
       </PopoverContent>
     </Popover>
   );
