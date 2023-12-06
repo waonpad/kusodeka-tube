fa1f16659e6d8b7aec921500f975d6be4f7d01d2
waonpad <waonpad@gmail.com>
2023-12-06 20:51:48 +0900

    fix: 🐞 close #58

--- apps/web/src/app/api/channels/route.ts
+++ apps/web/src/app/api/channels/route.ts
@@ -4,6 +4,8 @@ import { scalingChannel } from '@/utils/scaling-channel';
 import { cHandler } from '../_contracts/handler';
 import { getChannelByURLContract } from '../_contracts/routes/channels';
 
+export const dynamic = 'force-dynamic';
+
 export const GET = cHandler(getChannelByURLContract, async (req, { searchParams }) => {
   const channel = await youtubeApi.channels.get(searchParams!.url);
 
