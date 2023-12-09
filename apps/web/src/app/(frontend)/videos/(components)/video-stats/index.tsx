23719290b5e0cf96bd83b1306f191d1bc5f9ccb0
waonpad <waonpad@gmail.com>
2023-12-10 00:40:38 +0900

    feat: ✨ close #54

---
+++ apps/web/src/app/(frontend)/videos/(components)/video-stats/index.tsx
@@ -0,0 +1,54 @@
+import { cn } from 'ui/lib/utils';
+import { getVideoByURLContract } from '@/app/api/_contracts/routes/videos';
+
+export type VideoStats = React.HTMLAttributes<HTMLDivElement> & {
+  video: (typeof getVideoByURLContract.response._output)['video'];
+};
+
+export const VideoStats = ({ video, ...props }: VideoStats) => {
+  return (
+    <div {...props} className={cn('flex flex-col gap-2', props.className)}>
+      {/* 投稿日時 */}
+      <div className="flex items-center space-x-1 text-sm">
+        <span className="i-mdi-calendar-check h-4 w-4" />
+        <span>{video.snippet.publishedAt}</span>
+      </div>
+      {/* 再生数 */}
+      <div className="flex items-center space-x-1 text-sm">
+        <span className="i-mdi-play-circle-outline h-4 w-4" />
+        <div className="space-x-1">
+          <span>{Number(video.statistics.viewCount).toLocaleString()}</span>
+          <span className="text-xs">views</span>
+        </div>
+      </div>
+      {/* 高評価数 */}
+      <div className="flex items-center space-x-1 text-sm">
+        <span className="i-mdi-thumb-up-outline h-4 w-4" />
+        <div className="space-x-1">
+          <span>{Number(video.statistics.likeCount).toLocaleString()}</span>
+          <span className="text-xs">likes</span>
+        </div>
+      </div>
+      {/* コメント数 */}
+      <div className="flex items-center space-x-1 text-sm">
+        <span className="i-mdi-comment-outline h-4 w-4" />
+        <div className="space-x-1">
+          <span>{Number(video.statistics.commentCount).toLocaleString()}</span>
+          <span className="text-xs">comments</span>
+        </div>
+      </div>
+      {/* タグ */}
+      {/* TODO: タグの文字列で検索するリンクにしたら面白い？ */}
+      <div className="flex items-start space-x-1 text-sm">
+        <span className="i-mdi-bookmark-outline mt-[0.1rem] h-4 w-4 shrink-0" />
+        <div className="flex flex-wrap gap-1">
+          {video.snippet.tags?.map((tag, index) => (
+            <div key={index} className="rounded-sm bg-gray-100 px-2 py-0.5">
+              {tag}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
